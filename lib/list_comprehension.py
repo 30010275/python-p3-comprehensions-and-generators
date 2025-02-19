5c0d12252ad640de99701b7e62523ab7164fccb6
innocent <innocent.wasia@studentmoringaschool.com>
2025-02-19 09:35:14 +0300

    first commit

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,13 @@
-#!/usr/bin/env python3
+def return_evens(sequence):
+    """Returns a list of even numbers from the given sequence."""
+    return [num for num in sequence if num % 2 == 0]
 
-def return_evens(num_list):
-    pass
+def make_exclamation(sentences):
+    """Appends an exclamation mark to each sentence in the given list."""
+    return [sentence + "!" for sentence in sentences]
 
-def make_exclamation(sentence_list):
-    pass
+# Example test cases
+if __name__ == "__main__":
+    print(return_evens([0, 1, 3, 5, 7, 8, 9]))  # Output: [0, 8]
+    print(make_exclamation(["Hello", "I'm doing great", "Python is fun"]))
+    # Output: ["Hello!", "I'm doing great!", "Python is fun!"]
